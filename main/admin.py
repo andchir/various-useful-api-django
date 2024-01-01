7fac09c7c7713d0c12b0b69e54d1e8c62aec8626
andrew <andchir@gmail.com>
2024-01-01 17:52:44 +0300

    admin - delete image files

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,6 @@
+import os.path
 from django.contrib import admin
+from app import settings
 from main.models import ProductModel, ImageModel
 
 
@@ -13,3 +15,15 @@ class ImageModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'image', 'product')
     list_display_links = ('id', 'image', 'product')
 
+    def delete_model(self, request, obj):
+        image_file_path = os.path.join(settings.MEDIA_ROOT, str(obj.image))
+        if os.path.isfile(image_file_path):
+            os.remove(image_file_path)
+        obj.delete()
+
+    def delete_queryset(self, request, queryset):
+        for obj in queryset:
+            image_file_path = os.path.join(settings.MEDIA_ROOT, str(obj.image))
+            if os.path.isfile(image_file_path):
+                os.remove(image_file_path)
+        queryset.delete()
