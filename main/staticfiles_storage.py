687c6ca245493c0c44ad7c68b98d080ed6bb7711
andrew <andchir@gmail.com>
2024-01-04 00:41:42 +0300

    upd

---
+++ main/staticfiles_storage.py
@@ -0,0 +1,16 @@
+from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
+
+
+class NoSourceMapsStorage(ManifestStaticFilesStorage):
+    patterns = (
+        (
+            "*.css",
+            (
+                "(?P<matched>url\\(['\"]{0,1}\\s*(?P<url>.*?)[\"']{0,1}\\))",
+                (
+                    "(?P<matched>@import\\s*[\"']\\s*(?P<url>.*?)[\"'])",
+                    '@import url("%(url)s")',
+                ),
+            ),
+        ),
+    )
