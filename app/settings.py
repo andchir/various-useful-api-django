28ba5480649480a87ab2f6885d4237b7ffccf232
Andrei Chirko <andchir@gmail.com>
2024-01-20 18:50:59 +0300

    API for download video from YouTube

--- app/settings.py
+++ app/settings.py
@@ -215,8 +215,8 @@ SPECTACULAR_SETTINGS = {
     'SWAGGER_UI_DIST': 'SIDECAR',  # shorthand to use the sidecar instead
     'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
     'REDOC_DIST': 'SIDECAR',
-    'TITLE': 'Price monitoring API',
-    'DESCRIPTION': 'API for monitoring changes in prices for products.',
+    'TITLE': 'Various useful APIs',
+    'DESCRIPTION': 'Various useful APIs for api2app.ru.',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
 }
