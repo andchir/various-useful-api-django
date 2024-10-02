927274c152f311519e7ff760ece0a24233558ab7
andrew <andchir@gmail.com>
2024-10-02 22:08:44 +0300

    site_monitoring_log command

--- main/management/commands/site_monitoring.py
+++ main/management/commands/site_monitoring.py
@@ -12,29 +12,29 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         item_uuid = options['uuid'] if 'uuid' in options else None
-        if item_uuid is None:
-            self.stdout.write(self.style.ERROR('UUID is empty.'))
-            return
-        item = LogOwnerModel.objects.all().filter(uuid=item_uuid).first()
+        items = LogOwnerModel.objects.all().filter(uuid=item_uuid) \
+            if item_uuid \
+            else LogOwnerModel.objects.all()
 
-        if item is None:
+        if len(items) == 0:
             self.stdout.write(self.style.ERROR(f'Item not found ({item_uuid}).'))
             return
 
-        if item.site_url is None or item.data is None:
-            return
-        restart_command = item.data['restart_command'] if 'restart_command' in item.data else ''
-        if not restart_command:
-            return
-        site_host = item.site_url.replace('http://', '').replace('https://', '').split('/')[0]
-        ping_seconds = ping(site_host, timeout=5)
-        if ping_seconds is False:
-            self.stdout.write(self.style.ERROR(f"Ping {site_host}: FAIL"))
-            result = subprocess.run(restart_command.split(' '), capture_output=True, text=True)
-            log_item = LogItemModel(owner=item, name='CRASH', data={'restart_result': result.stdout})
-            log_item.save()
-            self.stdout.write(self.style.WARNING(result.stdout))
-        else:
-            self.stdout.write(self.style.WARNING(f"Ping {site_host}: {ping_seconds}"))
+        for item in items:
+            if item.site_url is None or item.data is None:
+                continue
+            restart_command = item.data['restart_command'] if 'restart_command' in item.data else ''
+            if not restart_command:
+                continue
+            site_host = item.site_url.replace('http://', '').replace('https://', '').split('/')[0]
+            ping_seconds = ping(site_host, timeout=5)
+            if ping_seconds is False:
+                self.stdout.write(self.style.ERROR(f"Ping {site_host}: FAIL"))
+                result = subprocess.run(restart_command.split(' '), capture_output=True, text=True)
+                log_item = LogItemModel(owner=item, name='CRASH', data={'restart_result': result.stdout})
+                log_item.save()
+                self.stdout.write(self.style.WARNING(result.stdout))
+            else:
+                self.stdout.write(self.style.WARNING(f"Ping {site_host}: {ping_seconds}"))
 
         self.stdout.write(self.style.SUCCESS('Done'))
